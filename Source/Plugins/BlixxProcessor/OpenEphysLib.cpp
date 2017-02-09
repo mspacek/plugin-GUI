4ba8d7cfb86822ede11c0d81f39b3c59b2c60823
Martin Spacek <git@mspacek.mm.st>
2017-02-09 13:13:10 +0100

    Make Blixx processor visible in list, tweak docs

--- Source/Plugins/BlixxProcessor/OpenEphysLib.cpp
+++ Source/Plugins/BlixxProcessor/OpenEphysLib.cpp
@@ -28,19 +28,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <Windows.h>
 #define EXPORT __declspec(dllexport)
 #else
-#define EXPORT
+#define EXPORT __attribute__((visibility("default")))
 #endif
 
 using namespace Plugin;
-//Number of plugins defined on the library. Can be of different types (Processors, RecordEngines, etc...)
+/* Number of plugins defined by the library. Can be of different types
+ * (Processors, RecordEngines, etc...) */
 #define NUM_PLUGINS 1
 
 extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
-    info->apiVersion = PLUGIN_API_VER; /*API version, defined by the GUI source.
-    Should not be changed to ensure it is always equal to the one used in the latest codebase. The GUI refueses to load plugins with mismatched API versions */
-    info->name = "Blixx library"; //Name of the Library, used only for information
-    info->libVersion = 1; //Version of the library, used only for information
+    /*API version, defined by the GUI source. Should not be changed to ensure it is always
+     * equal to the one used in the latest codebase. The GUI refueses to load plugins with
+     * mismatched API versions */
+    info->apiVersion = PLUGIN_API_VER;
+    info->name = "Blixx library"; // Name of the Library, used only for information
+    info->libVersion = 1; // Version of the library, used only for information
     info->numPlugins = NUM_PLUGINS;
 }
 
@@ -48,13 +51,12 @@ extern "C" EXPORT int getPluginInfo(int index, Plugin::PluginInfo* info)
 {
     switch (index)
     {
-    //one case per plugin. This example is for a processor which connects directly to the signal chain
     case 0:
-        info->type = Plugin::ProcessorPlugin; //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
-        //For processor
-        info->processor.name = "Blixx Processor"; //Processor name shown in the GUI
-        info->processor.type = Plugin::FilterProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
-        info->processor.creator = &(Plugin::createProcessor<BlixxProcessor>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
+        info->type = Plugin::PLUGIN_TYPE_PROCESSOR;
+        info->processor.name = "Blixx Processor"; // Processor name shown in the GUI
+        // Type of processor: FilterProcessor, SourceProcessor, SinkProcessor, UtilityProcessor
+        info->processor.type = Plugin::FilterProcessor;
+        info->processor.creator = &(Plugin::createProcessor<BlixxProcessor>); // Class factory pointer
         break;
 /**
 Examples for other plugin types
